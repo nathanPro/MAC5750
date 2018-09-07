10a17bcebf77e4ec0c02eb02da32e5f60d2d0261
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-09-07 20:30:56 -0300

    Guards and debug function at util.h

--- util.h
+++ util.h
@@ -1,11 +1,19 @@
+#ifndef UTIL_BCC
+#define UTIL_BCC
 #include "iostream"
 
-template<typename T>
-void write(std::ostream& out, T t) {
+template <typename T> void write(std::ostream& out, T t) {
     out << t << '\n';
 }
-template<typename T, typename... Args>
+template <typename T, typename... Args>
 void write(std::ostream& out, T t, Args... args) {
     out << t << ' ';
     write(out, args...);
 }
+
+template <typename... Args> void debug(Args... args) {
+#ifdef DEBUG
+    write(std::cerr, args...);
+#endif
+}
+#endif
