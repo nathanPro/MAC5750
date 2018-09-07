dc3eded598994fbaa480313d739bfc27291282b6
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-09-07 20:32:00 -0300

    Handling errors

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include "parser.h"
+#include "util.h"
 
 int main() {
     auto lexer = Lexer(std::cin);
-    auto r     = Parser::Exp(lexer);
+    try {
+        auto r = Parser::Exp(lexer);
+    } catch (Parser::UnexpectedEnd e) {
+        write(std::cerr, "BCC Error: File ended abruptly");
+        return 1;
+    } catch (Parser::Unexpected e) {
+        write(std::cerr, "BCC Error: Unexpected lexeme", e.lex,
+              "found");
+        return 1;
+    } catch (Parser::MismatchError e) {
+        write(std::cerr, "BCC Error: Mismatch: Expected", e.expected,
+              ", found", e.found);
+        return 1;
+    }
+    return 0;
 }
