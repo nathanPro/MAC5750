7e8d0c69119f2869f4e86a6e64334d1f4c911fa2
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-10-04 16:30:57 -0300

    Full IRBuilder testsuite

--- test/IRtest.cpp
+++ test/IRtest.cpp
@@ -2,8 +2,8 @@
 #include "gtest/gtest.h"
 
 class IRBuilderTest : public ::testing::Test {
-    protected:
-        IRBuilderTest() : tree() {}
+  protected:
+    IRBuilderTest() : tree() {}
 
     IR::Tree tree;
 };
@@ -39,8 +39,7 @@ TEST_F(IRBuilderTest, tempBuiltWithBuilder) {
 
 TEST_F(IRBuilderTest, binopBuiltWithBuilder) {
     IRBuilder builder(tree);
-    builder << IR::ExpId::BINOP
-            << IR::BinOp::PLUS << 10 << 11;
+    builder << IR::ExpId::BINOP << IR::Binop::PLUS << 10 << 11;
     auto ref = builder.build();
     EXPECT_EQ(tree.get_binop(ref).op, IR::BinOp::PLUS);
     EXPECT_EQ(tree.get_binop(ref).lhs, 10);
@@ -49,16 +48,14 @@ TEST_F(IRBuilderTest, binopBuiltWithBuilder) {
 
 TEST_F(IRBuilderTest, memBuiltWithBuilder) {
     IRBuilder builder(tree);
-    builder << IR::ExpId::MEM
-            << 32;
+    builder << IR::ExpId::MEM << 32;
     auto ref = builder.build();
     EXPECT_EQ(tree.get_mem(ref).exp, 32);
 }
 
 TEST_F(IRBuilderTest, callBuiltWithBuilder) {
     IRBuilder builder(tree);
-    builder << IR::ExpId::CALL
-            << 1 << 2;
+    builder << IR::ExpId::CALL << 1 << 2;
     auto ref = builder.build();
     EXPECT_EQ(tree.get_call(ref).func, 1);
     EXPECT_EQ(tree.get_call(ref).explist, 2);
@@ -66,11 +63,60 @@ TEST_F(IRBuilderTest, callBuiltWithBuilder) {
 
 TEST_F(IRBuilderTest, eseqBuiltWithBuilder) {
     IRBuilder builder(tree);
-    builder << IR::ExpId::ESEQ
-            << 1 << 2;
+    builder << IR::ExpId::ESEQ << 1 << 2;
     auto ref = builder.build();
     EXPECT_EQ(tree.get_eseq(ref).stm, 1);
     EXPECT_EQ(tree.get_eseq(ref).exp, 2);
+}
+
+TEST_F(IRBuilderTest, moveBuiltWithBuilder) {
+    IRBuilder builder(tree);
+    builder << IR::StmId::MOVE << 1 << 2;
+    auto ref = builder.build();
+    EXPECT_EQ(tree.get_move(ref).dst, 1);
+    EXPECT_EQ(tree.get_move(ref).src, 2);
+}
+
+TEST_F(IRBuilderTest, expBuiltWithBuilder) {
+    IRBuilder builder(tree);
+    builder << IR::StmId::EXP << 1;
+    auto ref = builder.build();
+    EXPECT_EQ(tree.get_exp(ref).exp, 1);
+}
+
+TEST_F(IRBuilderTest, jumpBuiltWithBuilder) {
+    IRBuilder builder(tree);
+    builder << IR::StmId::JUMP << 1 << 2;
+    auto ref = builder.build();
+    EXPECT_EQ(tree.get_jump(ref).exp, 1);
+    EXPECT_EQ(tree.get_jump(ref).targets, 2);
+}
+
+TEST_F(IRBuilderTest, cjumpBuiltWithBuilder) {
+    IRBuilder builder(tree);
+    builder << IR::StmId::CJUMP << IR::Relop::LESS << 2 << 3 << 32
+            << 36;
+    auto ref = builder.build();
+    EXPECT_EQ(tree.get_cjump(ref).op, IR::Relop::Less);
+    EXPECT_EQ(tree.get_cjump(ref).lhs, 2);
+    EXPECT_EQ(tree.get_cjump(ref).rhs, 2);
+    EXPECT_EQ(tree.get_cjump(ref).iftrue, 32);
+    EXPECT_EQ(tree.get_cjump(ref).iffalse, 36);
+}
+
+TEST_F(IRBuilderTest, seqBuiltWithBuilder) {
+    IRBuilder builder(tree);
+    builder << IR::StmId::SEQ << 13 << 15;
+    auto ref = builder.build();
+    EXPECT_EQ(tree.get_seq(ref).lhs, 13);
+    EXPECT_EQ(tree.get_seq(ref).rhs, 15);
+}
+
+TEST_F(IRBuilderTest, labelBuiltWithBuilder) {
+    IRBuilder builder(tree);
+    builder << IR::StmId::LABEL << 12;
+    auto ref = builder.build();
+    EXPECT_EQ(tree.get_label(ref).label, 12);
 }
 
 TEST_F(IRBuilderTest, treeKeepsType) {
@@ -96,6 +142,14 @@ TEST_F(IRBuilderTest, wrongGetNameThrows) {
     builder << IR::ExpId::CONST << 42;
     auto c = builder.build();
     EXPECT_THROW(tree.get_name(c), IR::BadAccess);
+}
+
+TEST_F(IRBuilderTest, stmAccessedAsExpThrows) {
+    IRBuilder builder(tree);
+    builder << IR::StmId::MOVE << 12 << 14;
+    auto stm = builder.build();
+    EXPECT_EQ(IR::ExpId::CONST, IR::StmId::MOVE);
+    EXPECT_THROW(tree.get_const(stm), IR::BadAccess);
 }
 
 int main(int argc, char** argv) {
