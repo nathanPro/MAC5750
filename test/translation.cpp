269b66163f8d86b381eeb4ac4406193e5bd93f0f
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-10-04 18:19:56 -0300

    Small test

--- test/translation.cpp
+++ test/translation.cpp
@@ -2,6 +2,7 @@
 #include "parser.h"
 #include "translate.h"
 #include "gtest/gtest.h"
+#include <sstream>
 
 class translatorTest : public ::testing::Test {
   protected:
@@ -15,6 +16,14 @@ class translatorTest : public ::testing::Test {
 };
 
 TEST_F(translatorTest, tuActuallyParses) { EXPECT_TRUE(no_err); }
+
+TEST_F(translatorTest, translateSumExp) {
+    std::stringstream exp_stream("3 + 4");
+    auto              ctx  = ParserContext(exp_stream);
+    auto              root = AST::translate(tree, Parser::Exp(ctx));
+    EXPECT_EQ(tree.get_type(root),
+              static_cast<int>(IR::ExpId::BINOP));
+}
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
