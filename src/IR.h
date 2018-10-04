d2e1a2293080fa69ef87ed39b0e88d068d2b7a66
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-10-04 16:45:33 -0300

    IR structure defined

--- src/IR.h
+++ src/IR.h
@@ -1,8 +1,8 @@
 #ifndef BCC_IR
 #define BCC_IR
 
-#include <vector>
 #include <cassert>
+#include <vector>
 
 class IRBuilder;
 namespace IR {
@@ -12,18 +12,9 @@ struct BadAccess {
     int expected;
 };
 
-enum ExpId
-{
-    CONST,
-    NAME,
-    TEMP,
-    BINOP,
-    MEM,
-    CALL,
-    ESEQ
-};
+enum RelopId { EQ, NE, LT, GT, LE, GE, ULT, ULE, UGT, UGE };
 
-enum BinOp {
+enum BinopId {
     PLUS,
     MINUS,
     MUL,
@@ -35,6 +26,9 @@ enum BinOp {
     ARSHIFT,
     XOR
 };
+
+enum ExpId { CONST, NAME, TEMP, BINOP, MEM, CALL, ESEQ };
+enum StmId { MOVE, EXP, JUMP, CJUMP, SEQ, LABEL };
 
 struct Const {
     int value;
@@ -68,11 +62,45 @@ struct Eseq {
     int exp;
 };
 
+struct Move {
+    int dst;
+    int src;
+};
+
+struct Exp {
+    int exp;
+};
+
+struct Jump {
+    int exp;
+    int targets;
+};
+
+struct Cjump {
+    int op;
+    int lhs;
+    int rhs;
+    int iftrue;
+    int iffalse;
+};
+
+struct Seq {
+    int lhs;
+    int rhs;
+};
+
+struct Label {
+    int label;
+};
+
 class Tree {
     friend class ::IRBuilder;
-    size_t id;
+    // Type checking info
+    size_t           id;
     std::vector<int> kind;
     std::vector<int> pos;
+
+    // Exp types
     std::vector<Const> _const;
     std::vector<Name>  _name;
     std::vector<Temp>  _temp;
@@ -81,8 +109,17 @@ class Tree {
     std::vector<Call>  _call;
     std::vector<Eseq>  _eseq;
 
+    // Stm types
+    std::vector<Move>  _move;
+    std::vector<Exp>   _exp;
+    std::vector<Jump>  _jump;
+    std::vector<Cjump> _cjump;
+    std::vector<Seq>   _seq;
+    std::vector<Label> _label;
+
   public:
     Tree() : id(0) {}
+    // Exp types
     Const& get_const(int ref);
     Name&  get_name(int ref);
     Temp&  get_temp(int ref);
@@ -90,9 +127,19 @@ class Tree {
     Mem&   get_mem(int ref);
     Call&  get_call(int ref);
     Eseq&  get_eseq(int ref);
-    int get_type(int ref);
+
+    // Stm types
+    Move&  get_move(int ref);
+    Exp&   get_exp(int ref);
+    Jump&  get_jump(int ref);
+    Cjump& get_cjump(int ref);
+    Seq&   get_seq(int ref);
+    Label& get_label(int ref);
+
+    // generic functinality
+    int    get_type(int ref);
     size_t size() const;
 };
-}
+} // namespace IR
 
 #endif
