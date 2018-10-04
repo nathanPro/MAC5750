0368f3599a30d489f8f3c09aa4ca34c6fbfd69f7
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-10-04 01:55:39 -0300

    IR Tree exp defined

--- src/IR.cpp
+++ src/IR.cpp
@@ -6,15 +6,21 @@ static void check_for_exp(int found, int expected) {
 }
 
 namespace IR {
-    Const& Tree::get_const(int ref) {
-        check_for_exp(kind[ref], IR::ExpId::CONST);
-        return _const[pos[ref]];
+#define IR_GETTER(name, ID, ret) \
+    ret& Tree::get ## name (int ref) { \
+        check_for_exp(kind[ref], ID); \
+        return name[pos[ref]]; \
     }
 
-    Name& Tree::get_name(int ref) {
-        check_for_exp(kind[ref], IR::ExpId::NAME);
-        return _name[pos[ref]];
-    }
+    IR_GETTER(_const, ExpId::CONST, Const)
+    IR_GETTER(_name,  ExpId::NAME,  Name)
+    IR_GETTER(_temp,  ExpId::TEMP,  Temp)
+    IR_GETTER(_binop, ExpId::BINOP, Binop)
+    IR_GETTER(_mem,   ExpId::MEM,     Mem)
+    IR_GETTER(_call,  ExpId::CALL,   Call)
+    IR_GETTER(_eseq,  ExpId::ESEQ,   Eseq)
+
+#undef IR_GETTER
 
     size_t Tree::size() const {
         return id;
--- src/IR.h
+++ src/IR.h
@@ -23,6 +23,19 @@ enum ExpId
     ESEQ
 };
 
+enum BinOp {
+    PLUS,
+    MINUS,
+    MUL,
+    DIV,
+    AND,
+    OR,
+    LSHIFT,
+    RSHIFT,
+    ARSHIFT,
+    XOR
+};
+
 struct Const {
     int value;
 };
@@ -31,18 +44,52 @@ struct Name {
     int label;
 };
 
+struct Temp {
+    int id;
+};
+
+struct Binop {
+    int op;
+    int lhs;
+    int rhs;
+};
+
+struct Mem {
+    int exp;
+};
+
+struct Call {
+    int func;
+    int explist;
+};
+
+struct Eseq {
+    int stm;
+    int exp;
+};
+
 class Tree {
     friend class ::IRBuilder;
     size_t id;
     std::vector<int> kind;
     std::vector<int> pos;
     std::vector<Const> _const;
-    std::vector<Name> _name;
+    std::vector<Name>  _name;
+    std::vector<Temp>  _temp;
+    std::vector<Binop> _binop;
+    std::vector<Mem>   _mem;
+    std::vector<Call>  _call;
+    std::vector<Eseq>  _eseq;
 
   public:
     Tree() : id(0) {}
     Const& get_const(int ref);
-    Name& get_name(int ref);
+    Name&  get_name(int ref);
+    Temp&  get_temp(int ref);
+    Binop& get_binop(int ref);
+    Mem&   get_mem(int ref);
+    Call&  get_call(int ref);
+    Eseq&  get_eseq(int ref);
     int get_type(int ref);
     size_t size() const;
 };
