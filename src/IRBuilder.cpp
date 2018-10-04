fff0a16ace771d102112468e47a0c1ff8ccac32e
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-10-04 02:02:30 -0300

    IRBuilder works with exp

--- src/IRBuilder.cpp
+++ src/IRBuilder.cpp
@@ -17,13 +17,34 @@ int IRBuilder::build() {
     ref = base.id++;
     switch (kind) {
         case IR::ExpId::CONST:
-            base.pos.emplace_back(base._const.size());
+            base.pos.push_back(base._const.size());
             base._const.push_back(IR::Const{data[0]});
-        break;
+            break;
         case IR::ExpId::NAME:
-            base.pos.emplace_back(base._name.size());
+            base.pos.push_back(base._name.size());
             base._name.push_back(IR::Name{data[0]});
-        break;
+            break;
+        case IR::ExpId::TEMP:
+            base.pos.push_back(base._temp.size());
+            base._temp.push_back(IR::Temp{data[0]});
+            break;
+        case IR::ExpId::BINOP:
+            base.pos.push_back(base._binop.size());
+            base._binop.push_back(IR::Binop{data[0],
+                                            data[1], data[2]});
+            break;
+        case IR::ExpId::MEM:
+            base.pos.push_back(base._mem.size());
+            base._mem.push_back(IR::Mem{data[0]});
+            break;
+        case IR::ExpId::CALL:
+            base.pos.push_back(base._call.size());
+            base._call.push_back(IR::Call{data[0], data[1]});
+            break;
+        case IR::ExpId::ESEQ:
+            base.pos.push_back(base._eseq.size());
+            base._eseq.push_back(IR::Eseq{data[0], data[1]});
+            break;
     }
     return ref;
 }
