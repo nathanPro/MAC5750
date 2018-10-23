b0af9bdec56640f8fd2815bbe42f73e240a97856
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-10-23 11:09:36 -0300

    Lexer without template argument

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,6 +1,4 @@
 #include "lexer.h"
-#include "util.h"
-#include <algorithm>
 
 Trie::Trie(const std::vector<std::string>& words) {
     root = std::make_unique<Trie::Node>();
@@ -16,4 +14,80 @@ Trie::Trie(const std::vector<std::string>& words) {
         node->child[0]->label = static_cast<Lexeme>(
             static_cast<int>(Lexeme::boolean_keyword) + i);
     }
+}
+
+Lexer::Lexer(std::istream* _in, size_t _la)
+    : symbols(reserved_words), in(_in), lo(0), la(_la), lc(0), pos(0),
+      LA(la) {
+    for (size_t i = 0; i < la; i++) LA[i] = advance();
+}
+
+LexState Lexer::operator[](size_t i) { return LA[(pos + i) % la]; }
+
+LexState Lexer::advance() {
+    if (LA[pos].first == Lexeme::eof) return LA[pos];
+    while (true) {
+        if (lo == line.size()) {
+            std::getline(*in, line);
+            lo = 0;
+            if (!in->good()) return {Lexeme::eof, std::string(), lc};
+            lc++;
+        }
+        while (lo < line.size()) {
+            while (lo < line.size() && isspace(line[lo])) lo++;
+            if (lo == line.size()) break;
+
+            if (ispunct(line[lo])) {
+                // Still need to fix /* */ comment style
+                std::string word(line, lo, 2);
+                auto        lex = symbols.search(word.c_str());
+                if (lex == Lexeme::identifier) {
+                    word = std::string(line, lo, 1);
+                    lex  = symbols.search(word.c_str());
+                } else if (lex == Lexeme::inline_comment) {
+                    lo = line.size();
+                    continue; // NON-OBVIOUS CONTROL FLOW
+                }
+                lo += word.size();
+                return {lex, word, lc};
+            }
+
+            if (isdigit(line[lo])) {
+                size_t hi =
+                    consume([](char c) { return isdigit(c); });
+                std::string word(line, lo, hi - lo);
+                lo += word.size();
+                return {Lexeme::integer_literal, word, lc};
+            }
+
+            if (isalpha(line[lo])) {
+                std::string word(line, lo, 18);
+                auto        lex = symbols.search(word.c_str());
+                if (lex == Lexeme::println_keyword) {
+                    lo += 18;
+                    return {lex, word, lc};
+                }
+
+                size_t hi = consume(
+                    [](char c) { return isalnum(c) || c == '_'; });
+                word = std::string(line, lo, hi - lo);
+                lex  = symbols.search(word.c_str());
+                lo += word.size();
+                return {lex, word, lc};
+            }
+            __builtin_unreachable();
+        }
+    }
+}
+
+bool Lexer::empty() const { return LA[pos].first == Lexeme::eof; }
+
+size_t Lexer::line_count() const { return LA[pos].third; }
+
+LexState Lexer::operator*() { return LA[pos]; }
+
+Lexer& Lexer::operator++() {
+    LA[pos] = advance();
+    pos     = (pos + 1) % la;
+    return *this;
 }
--- src/lexer.h
+++ src/lexer.h
@@ -2,6 +2,7 @@
 #define BCC_LEXER
 
 #include <cinttypes>
+#include <istream>
 #include <limits>
 #include <memory>
 #include <string>
@@ -112,7 +113,7 @@ ostream& operator<<(ostream& out, Lexeme lex) {
 class Trie {
     template <typename T> using ptr = std::unique_ptr<T>;
     struct Node {
-        Lexeme label = Lexeme::identifier;
+        Lexeme    label = Lexeme::identifier;
         ptr<Node> child[1 + std::numeric_limits<char>::max()];
     };
     ptr<Node> root;
@@ -123,30 +124,30 @@ class Trie {
 };
 
 struct LexState {
-    Lexeme first;
+    Lexeme      first;
     std::string second;
-    size_t third;
+    size_t      third;
 
     explicit operator Lexeme() { return first; }
 };
 
-template <typename istream> class Lexer {
-    Trie symbols;
-    istream& in;
-    size_t lo, la, lc, pos;
-    std::string line;
+class Lexer {
+    Trie                  symbols;
+    std::istream*         in;
+    size_t                lo, la, lc, pos;
+    std::string           line;
     std::vector<LexState> LA;
 
     template <typename F> size_t consume(F f);
-    LexState advance();
+    LexState                     advance();
 
   public:
-    Lexer(istream&, size_t lookahead = 1);
-    bool empty() const;
+    Lexer(std::istream*, size_t lookahead = 1);
+    bool     empty() const;
     LexState operator*();
     LexState operator[](size_t i);
-    size_t line_count() const;
-    Lexer& operator++();
+    size_t   line_count() const;
+    Lexer&   operator++();
 };
 
 template <typename It> Lexeme Trie::search(It q) const {
@@ -161,99 +162,9 @@ template <typename It> Lexeme Trie::search(It q) const {
     return node->child[0]->label;
 }
 
-template <typename istream>
-Lexer<istream>::Lexer(istream& _in, size_t _la)
-    : symbols(reserved_words), in(_in), lo(0), la(_la), lc(0), pos(0),
-      LA(la) {
-    for (size_t i = 0; i < la; i++) LA[i] = advance();
-}
-
-template <typename istream>
-LexState Lexer<istream>::operator[](size_t i) {
-    return LA[(pos + i) % la];
-}
-
-template <typename istream>
-template <typename F>
-size_t Lexer<istream>::consume(F f) {
+template <typename F> size_t Lexer::consume(F f) {
     size_t hi = lo;
     while (hi < line.size() && f(line[hi])) hi++;
     return hi;
-}
-
-template <typename istream> LexState Lexer<istream>::advance() {
-    if (LA[pos].first == Lexeme::eof) return LA[pos];
-    while (true) {
-        if (lo == line.size()) {
-            std::getline(in, line);
-            lo = 0;
-            if (!in.good()) return {Lexeme::eof, std::string(), lc};
-            lc++;
-        }
-        while (lo < line.size()) {
-            while (lo < line.size() && isspace(line[lo])) lo++;
-            if (lo == line.size()) break;
-
-            if (ispunct(line[lo])) {
-                // Still need to fix /* */ comment style
-                std::string word(line, lo, 2);
-                auto lex = symbols.search(word.c_str());
-                if (lex == Lexeme::identifier) {
-                    word = std::string(line, lo, 1);
-                    lex  = symbols.search(word.c_str());
-                } else if (lex == Lexeme::inline_comment) {
-                    lo = line.size();
-                    continue; // NON-OBVIOUS CONTROL FLOW
-                }
-                lo += word.size();
-                return {lex, word, lc};
-            }
-
-            if (isdigit(line[lo])) {
-                size_t hi =
-                    consume([](char c) { return isdigit(c); });
-                std::string word(line, lo, hi - lo);
-                lo += word.size();
-                return {Lexeme::integer_literal, word, lc};
-            }
-
-            if (isalpha(line[lo])) {
-                std::string word(line, lo, 18);
-                auto lex = symbols.search(word.c_str());
-                if (lex == Lexeme::println_keyword) {
-                    lo += 18;
-                    return {lex, word, lc};
-                }
-
-                size_t hi = consume(
-                    [](char c) { return isalnum(c) || c == '_'; });
-                word = std::string(line, lo, hi - lo);
-                lex  = symbols.search(word.c_str());
-                lo += word.size();
-                return {lex, word, lc};
-            }
-            __builtin_unreachable();
-        }
-    }
-}
-
-template <typename istream> bool Lexer<istream>::empty() const {
-    return LA[pos].first == Lexeme::eof;
-}
-
-template <typename istream>
-size_t Lexer<istream>::line_count() const {
-    return LA[pos].third;
-}
-
-template <typename istream> LexState Lexer<istream>::operator*() {
-    return LA[pos];
-}
-
-template <typename istream>
-Lexer<istream>& Lexer<istream>::operator++() {
-    LA[pos] = advance();
-    pos     = (pos + 1) % la;
-    return *this;
 }
 #endif
