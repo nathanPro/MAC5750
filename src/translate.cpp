86327193e6004ffabe01d9c67e08dc5f4af1f579
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-12-12 21:21:46 -0200

    sum translation done

--- src/translate.cpp
+++ src/translate.cpp
@@ -4,25 +4,39 @@ namespace IR
 {
 int translate(Tree& tree, AST::Exp const& exp)
 {
-    auto vis = Util::type_switch{
-
-        [&](AST::sumExp const& sum) {
-            IRBuilder builder(tree);
+    struct Visitor {
+        Tree& t;
+        int   operator()(AST::sumExp const& sum)
+        {
+            IRBuilder builder(t);
             builder << IRTag::BINOP << IR::BinopId::PLUS
-                    << translate(tree, sum.lhs)
-                    << translate(tree, sum.rhs);
+                    << Grammar::visit(*this, sum.lhs)
+                    << Grammar::visit(*this, sum.rhs);
             return builder.build();
-        },
-
-        [&](AST::integerExp const& exp) {
-            IRBuilder builder(tree);
+        }
+        int operator()(AST::integerExp const& exp)
+        {
+            IRBuilder builder(t);
             builder << IRTag::CONST << exp.value;
             return builder.build();
-        },
-
-        [&](auto&&) { return -1; }
-
+        }
+        int operator()(AST::andExp const&) { return -1; }
+        int operator()(AST::lessExp const&) { return -1; }
+        int operator()(AST::minusExp const&) { return -1; }
+        int operator()(AST::prodExp const&) { return -1; }
+        int operator()(AST::indexingExp const&) { return -1; }
+        int operator()(AST::lengthExp const&) { return -1; }
+        int operator()(AST::methodCallExp const&) { return -1; }
+        int operator()(AST::trueExp const&) { return -1; }
+        int operator()(AST::falseExp const&) { return -1; }
+        int operator()(AST::thisExp const&) { return -1; }
+        int operator()(AST::identifierExp const&) { return -1; }
+        int operator()(AST::newArrayExp const&) { return -1; }
+        int operator()(AST::newObjectExp const&) { return -1; }
+        int operator()(AST::bangExp const&) { return -1; }
+        int operator()(AST::parenExp const&) { return -1; }
+        int operator()(AST::ExpListRule const&) { return -1; }
     };
-    return Grammar::visit(vis, exp);
+    return Grammar::visit(Visitor{tree}, exp);
 }
 } // namespace IR
