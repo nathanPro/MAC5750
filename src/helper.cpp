c4195a5a18822f37dc5355b7ad1424fb3dfa0251
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-12-12 02:21:14 -0200

    Topological sort classes

--- src/helper.cpp
+++ src/helper.cpp
@@ -3,6 +3,81 @@
 namespace helper
 {
 
+class_graph::class_graph(AST::ProgramRule const& prog) : cnt(0)
+{
+    {
+        auto nc = class_graph::name_collector{*this};
+        nc(prog);
+    }
+    n = names.size();
+    G.resize(n);
+    seen.resize(n);
+    finished.resize(n);
+    ans.reserve(n);
+    {
+        auto ec = class_graph::edge_collector{*this};
+        ec(prog);
+    }
+    for (int i = 0; i < n; i++)
+        if (!seen[i]) dfs(i);
+    std::reverse(begin(ans), end(ans));
+}
+
+void class_graph::dfs(int i)
+{
+    seen[i] = true;
+    for (auto j : G[i]) {
+        if (seen[j] && !finished[j])
+            throw helper::cyclic_classes{};
+        else if (!seen[j])
+            dfs(j);
+    }
+    finished[i] = true;
+    ans.push_back(i);
+}
+
+void class_graph::name_collector::
+     operator()(AST::ProgramRule const& prog)
+{
+    Grammar::visit(*this, prog.main);
+    for (auto const& cls : prog.classes) Grammar::visit(*this, cls);
+}
+
+void class_graph::edge_collector::
+     operator()(AST::ProgramRule const& prog)
+{
+    for (auto const& cls : prog.classes) Grammar::visit(*this, cls);
+}
+
+void class_graph::edge_collector::init_vars(
+    int i, std::vector<AST::VarDecl> const& vars)
+{
+    for (auto const& var : vars) {
+        auto const& vdr = Grammar::get<AST::VarDeclRule>(var);
+        if (Grammar::holds<AST::classType>(vdr.type)) {
+            std::string const& type_name =
+                Grammar::get<AST::classType>(vdr.type).value;
+            int const j = cg.names.at(type_name);
+            cg.G[j].push_back(i);
+        }
+    }
+}
+
+void class_graph::edge_collector::
+     operator()(AST::ClassDeclInheritance const& cls)
+{
+    int const i = cg.names.at(cls.name);
+    int const j = cg.names.at(cls.superclass);
+    cg.G[j].push_back(i);
+    init_vars(i, cls.variables);
+}
+
+void class_graph::edge_collector::
+     operator()(AST::ClassDeclNoInheritance const& cls)
+{
+    init_vars(cg.names.at(cls.name), cls.variables);
+}
+
 class_spec::class_spec(meta_data const& d, const AST::MainClassRule&)
     : method_cnt(0), data(d), base(-1)
 {
@@ -14,7 +89,7 @@ class_spec::class_spec(meta_data const& d, const AST::MainClassRule&)
 void class_spec::init_vars(std::vector<AST::VarDecl> const& vars)
 {
     int tot = 0;
-    for (const auto& var : vars) {
+    for (auto const& var : vars) {
         auto const& vdr  = Grammar::get<AST::VarDeclRule>(var);
         kind[vdr.name]   = kind_t::var;
         layout[vdr.name] = tot;
@@ -65,8 +140,12 @@ meta_data::meta_data(const AST::Program& prog)
 
 void meta_data::operator()(const AST::ProgramRule& prog)
 {
-    Grammar::visit(*this, prog.main);
-    for (auto const& cls : prog.classes) Grammar::visit(*this, cls);
+    class_graph top_sort(prog);
+    for (int i : top_sort.ans)
+        if (i == 0)
+            Grammar::visit(*this, prog.main);
+        else
+            Grammar::visit(*this, prog.classes[i - 1]);
 }
 
 void meta_data::operator()(const AST::MainClassRule& cls)
