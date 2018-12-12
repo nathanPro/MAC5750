3a17ac9d1028b5c11d405ba59bb99d74229e8328
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-12-12 21:21:34 -0200

    Improved translation test

--- test/translation.cpp
+++ test/translation.cpp
@@ -11,7 +11,25 @@ TEST(translatorTest, translateSum)
     auto     ir     = IR::translate(tree, Parser(&stream).Exp());
     ASSERT_NE(ir, -1);
     EXPECT_EQ(tree.get_type(ir), IR::IRTag::BINOP);
+    auto [op, lhs, rhs] = tree.get_binop(ir);
+    EXPECT_EQ(op, IR::BinopId::PLUS);
+    ASSERT_NE(lhs, -1);
+    ASSERT_NE(rhs, -1);
+    EXPECT_EQ(tree.get_type(lhs), IR::IRTag::CONST);
+    EXPECT_EQ(tree.get_type(rhs), IR::IRTag::CONST);
+    EXPECT_EQ(tree.get_const(lhs).value, 3);
+    EXPECT_EQ(tree.get_const(rhs).value, 4);
 }
+
+TEST(translatorTest, translateProd)
+{
+    IR::Tree tree;
+    auto     stream = std::stringstream("3 * (4 + 2)\n");
+    auto     ir     = IR::translate(tree, Parser(&stream).Exp());
+    ASSERT_NE(ir, -1);
+    EXPECT_EQ(tree.get_type(ir), IR::IRTag::BINOP);
+}
+
 int main(int argc, char** argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
