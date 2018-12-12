e37b5a948bbeff161ba7909f620f533ee96bb2db
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2018-12-12 02:21:32 -0200

    Another wrapper for std::variant

--- src/grammar.h
+++ src/grammar.h
@@ -5,30 +5,43 @@
 #include <utility>
 #include <variant>
 
-namespace Grammar {
+namespace Grammar
+{
 template <typename variant> struct Nonterminal {
     using variant_t = variant;
 
     template <typename T>
     Nonterminal(T&& in)
-        : _self(std::make_unique<variant_t>(std::move(in))) {}
+        : _self(std::make_unique<variant_t>(std::move(in)))
+    {
+    }
     template <class Visitor, class Nonterminal>
     friend constexpr auto visit(Visitor&& vis, Nonterminal&& nt);
     template <typename Alternative, class Nonterminal>
     friend constexpr const Alternative& get(Nonterminal&&);
+    template <typename Alternative, class Nonterminal>
+    friend constexpr bool holds(Nonterminal&&);
 
   private:
     std::unique_ptr<variant_t> _self;
 };
 
 template <class Visitor, class Nonterminal>
-constexpr auto visit(Visitor&& vis, Nonterminal&& nt) {
+constexpr auto visit(Visitor&& vis, Nonterminal&& nt)
+{
     return std::visit(std::forward<Visitor>(vis), *nt._self);
 }
 
 template <typename Alternative, class Nonterminal>
-constexpr const Alternative& get(Nonterminal&& nt) {
+constexpr const Alternative& get(Nonterminal&& nt)
+{
     return std::get<Alternative>(*nt._self);
+}
+
+template <typename Alternative, class Nonterminal>
+constexpr bool holds(Nonterminal&& nt)
+{
+    return std::holds_alternative<Alternative>(*nt._self);
 }
 
 struct Indexable {
