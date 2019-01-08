3964342206aede3736ec261e3193267b35d467fb
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2019-01-08 18:00:20 -0200

    Created translate visitor

--- src/translate.cpp
+++ src/translate.cpp
@@ -2,131 +2,124 @@
 
 namespace IR
 {
-int translate(Tree& tree, AST::Exp const& exp)
+
+Translator::Translator(Tree& tree) : t(tree) {}
+
+int Translator::binop(BinopId                                    id,
+                      AST::__detail::BinaryRule<AST::Exp> const& exp)
 {
-    struct Visitor {
-        Tree& t;
-
-        int binop(IR::BinopId                                id,
-                  AST::__detail::BinaryRule<AST::Exp> const& exp)
-        {
-            IRBuilder ans(t);
-            ans << IRTag::BINOP << id
-                << Grammar::visit(*this, exp.lhs)
-                << Grammar::visit(*this, exp.rhs);
-            return ans.build();
-        }
-
-        int operator()(AST::andExp const& exp)
-        {
-            return binop(IR::BinopId::AND, exp);
-        }
-
-        int operator()(AST::sumExp const& exp)
-        {
-            return binop(IR::BinopId::PLUS, exp);
-        }
-
-        int operator()(AST::minusExp const& exp)
-        {
-            return binop(IR::BinopId::MINUS, exp);
-        }
-
-        int operator()(AST::prodExp const& exp)
-        {
-            return binop(IR::BinopId::MUL, exp);
-        }
-
-        int operator()(AST::integerExp const& exp)
-        {
-            IRBuilder builder(t);
-            builder << IRTag::CONST << exp.value;
-            return builder.build();
-        }
-
-        int operator()(AST::trueExp const&)
-        {
-            IRBuilder builder(t);
-            builder << IRTag::CONST << 1;
-            return builder.build();
-        }
-
-        int operator()(AST::falseExp const&)
-        {
-            IRBuilder builder(t);
-            builder << IRTag::CONST << 0;
-            return builder.build();
-        }
-
-        int operator()(AST::parenExp const& exp)
-        {
-            return Grammar::visit(*this, exp.inner);
-        }
-
-        int operator()(AST::lessExp const& exp)
-        {
-            IRBuilder root(t);
-            IRBuilder tmp(t);
-            IRBuilder cmp(t);
-
-            tmp << IRTag::TEMP;
-            cmp << IRTag::CMP << Grammar::visit(*this, exp.lhs)
-                << Grammar::visit(*this, exp.rhs);
-
-            root << IRTag::MOVE << tmp.build() << cmp.build();
-            return root.build();
-        }
-
-        int operator()(AST::bangExp const& exp)
-        {
-            auto mv  = Grammar::visit(*this, exp.inner);
-            auto tmp = t.get_move(mv).dst;
-
-            IRBuilder root(t);
-            IRBuilder cte(t);
-            cte << IR::IRTag::CONST << 1;
-
-            root << IR::IRTag::BINOP << IR::BinopId::XOR << tmp
-                 << cte.build();
-
-            return root.build();
-        }
-
-        int operator()(AST::ExpListRule const&) { return -1; }
-
-        int operator()(AST::lengthExp const&) { return -1; }
-        int operator()(AST::methodCallExp const&) { return -1; }
-
-        int operator()(AST::thisExp const&) { return -1; }
-        int operator()(AST::identifierExp const&) { return -1; }
-        int operator()(AST::indexingExp const&) { return -1; }
-
-        int operator()(AST::newArrayExp const&) { return -1; }
-        int operator()(AST::newObjectExp const&) { return -1; }
-    };
-    return Grammar::visit(Visitor{tree}, exp);
+    IRBuilder ans(t);
+    ans << IRTag::BINOP << id << Grammar::visit(*this, exp.lhs)
+        << Grammar::visit(*this, exp.rhs);
+    return ans.build();
 }
 
-int translate(Tree& tree, AST::Stm const& stm)
+int Translator::operator()(AST::andExp const& exp)
 {
-    struct Visitor {
-        Tree& t;
+    return binop(IR::BinopId::AND, exp);
+}
 
-        int operator()(AST::blockStm const&) { return -1; }
-        int operator()(AST::ifStm const&) { return -1; }
-        int operator()(AST::whileStm const&) { return -1; }
-        int operator()(AST::printStm const& stm)
-        {
-            IRBuilder builder(t);
+int Translator::operator()(AST::sumExp const& exp)
+{
+    return binop(IR::BinopId::PLUS, exp);
+}
 
-            builder << IR::IRTag::CALL << 0;
-            builder << t.keep_explist(Explist{translate(t, stm.exp)});
-            return builder.build();
-        }
-        int operator()(AST::assignStm const&) { return -1; }
-        int operator()(AST::indexAssignStm const&) { return -1; }
-    };
-    return Grammar::visit(Visitor{tree}, stm);
+int Translator::operator()(AST::minusExp const& exp)
+{
+    return binop(IR::BinopId::MINUS, exp);
+}
+
+int Translator::operator()(AST::prodExp const& exp)
+{
+    return binop(IR::BinopId::MUL, exp);
+}
+
+int Translator::operator()(AST::integerExp const& exp)
+{
+    IRBuilder builder(t);
+    builder << IRTag::CONST << exp.value;
+    return builder.build();
+}
+
+int Translator::operator()(AST::trueExp const&)
+{
+    IRBuilder builder(t);
+    builder << IRTag::CONST << 1;
+    return builder.build();
+}
+
+int Translator::operator()(AST::falseExp const&)
+{
+    IRBuilder builder(t);
+    builder << IRTag::CONST << 0;
+    return builder.build();
+}
+
+int Translator::operator()(AST::parenExp const& exp)
+{
+    return Grammar::visit(*this, exp.inner);
+}
+
+int Translator::operator()(AST::lessExp const& exp)
+{
+    IRBuilder root(t);
+    IRBuilder tmp(t);
+    IRBuilder cmp(t);
+
+    tmp << IRTag::TEMP;
+    cmp << IRTag::CMP << Grammar::visit(*this, exp.lhs)
+        << Grammar::visit(*this, exp.rhs);
+
+    root << IRTag::MOVE << tmp.build() << cmp.build();
+    return root.build();
+}
+
+int Translator::operator()(AST::bangExp const& exp)
+{
+    auto mv  = Grammar::visit(*this, exp.inner);
+    auto tmp = t.get_move(mv).dst;
+
+    IRBuilder root(t);
+    IRBuilder cte(t);
+    cte << IR::IRTag::CONST << 1;
+
+    root << IR::IRTag::BINOP << IR::BinopId::XOR << tmp
+         << cte.build();
+
+    return root.build();
+}
+
+int Translator::operator()(AST::ExpListRule const&) { return -1; }
+int Translator::operator()(AST::lengthExp const&) { return -1; }
+int Translator::operator()(AST::methodCallExp const&) { return -1; }
+int Translator::operator()(AST::thisExp const&) { return -1; }
+int Translator::operator()(AST::identifierExp const&) { return -1; }
+int Translator::operator()(AST::indexingExp const&) { return -1; }
+int Translator::operator()(AST::newArrayExp const&) { return -1; }
+int Translator::operator()(AST::newObjectExp const&) { return -1; }
+int Translator::operator()(AST::blockStm const&) { return -1; }
+int Translator::operator()(AST::ifStm const&) { return -1; }
+int Translator::operator()(AST::whileStm const&) { return -1; }
+int Translator::operator()(AST::printStm const& stm)
+{
+    IRBuilder builder(t);
+
+    builder << IR::IRTag::CALL << 0;
+    builder << t.keep_explist(
+        Explist{Grammar::visit(*this, stm.exp)});
+    return builder.build();
+}
+int Translator::operator()(AST::assignStm const&) { return -1; }
+int Translator::operator()(AST::indexAssignStm const&) { return -1; }
+
+int translate(Tree& t, AST::Exp const& exp)
+{
+    return Grammar::visit(Translator{t}, exp);
+}
+
+int translate(Tree& t, AST::Stm const& stm)
+{
+    return Grammar::visit(Translator{t}, stm);
 }
 
 } // namespace IR
--- src/translate.h
+++ src/translate.h
@@ -7,6 +7,41 @@
 
 namespace IR
 {
+class Translator
+{
+    Tree& t;
+
+    int binop(BinopId, AST::__detail::BinaryRule<AST::Exp> const&);
+
+  public:
+    Translator(Tree&);
+
+    int operator()(AST::andExp const& exp);
+    int operator()(AST::sumExp const& exp);
+    int operator()(AST::minusExp const& exp);
+    int operator()(AST::prodExp const& exp);
+    int operator()(AST::integerExp const& exp);
+    int operator()(AST::trueExp const&);
+    int operator()(AST::falseExp const&);
+    int operator()(AST::parenExp const& exp);
+    int operator()(AST::lessExp const& exp);
+    int operator()(AST::bangExp const& exp);
+    int operator()(AST::ExpListRule const&);
+    int operator()(AST::lengthExp const&);
+    int operator()(AST::methodCallExp const&);
+    int operator()(AST::thisExp const&);
+    int operator()(AST::identifierExp const&);
+    int operator()(AST::indexingExp const&);
+    int operator()(AST::newArrayExp const&);
+    int operator()(AST::newObjectExp const&);
+    int operator()(AST::blockStm const&);
+    int operator()(AST::ifStm const&);
+    int operator()(AST::whileStm const&);
+    int operator()(AST::printStm const& stm);
+    int operator()(AST::assignStm const&);
+    int operator()(AST::indexAssignStm const&);
+};
+
 int translate(Tree&, AST::Exp const&);
 int translate(Tree&, AST::Stm const&);
 } // namespace IR
