2403e9ce44062f1ee88a655cac432fba376ae161
Nathan Benedetto Proença <nathan@vieiraproenca.com>
2019-01-20 14:14:21 -0200

    Making lab more convenient

--- src/lab.cpp
+++ src/lab.cpp
@@ -15,9 +15,24 @@ int main(int argc, char** argv)
         return 1;
     }
 
+    bool debug = false;
+    for (int i = 3; i < argc; i++)
+        if (argv[i][0] == 'd') debug = true;
+
     TranslationUnit tu(std::string{argv[1]});
     IR::Tree        tree;
     translate(tree, tu.syntax_tree);
-    GEN::codegen code(&std::cout, tree);
-    tree.dump(std::cout);
+
+    if (debug) {
+        IR::Tree cp = tree;
+        Util::write(std::cerr, "Base Tree");
+        cp.dump(std::cerr);
+        cp.simplify();
+        Util::write(std::cerr, "Simplified Tree");
+        cp.dump(std::cerr);
+    }
+
+    std::ofstream out(argv[2]);
+    GEN::codegen  code(&out, tree);
+    tree.dump(out);
 }
